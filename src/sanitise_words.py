524b3d9ff664ff0315a0a2fdc286a60ea7b602d5
Paulo Azoia <pazoia.gomes@gmail.com>
2022-01-25 21:59:53 +0000

    refactored sanitise word ro deal hyphens

--- src/sanitise_words.py
+++ src/sanitise_words.py
@@ -3,18 +3,21 @@ from string import punctuation
 
 class SanitiseWord:
     def __init__(self):
-        pass
+        self.sanitised_word = ""
 
     def sanitise_word(self, word):
         valid_word = word.isalpha()
+        
         if not valid_word:
+            word = re.sub(r"\-", " ", word)
             word = re.sub(r"\'s", "", word)
             word = re.sub(r"\'ve", "", word)
-            word = re.sub(r"n\'t", "", word)
+            word = re.sub(r"\'t", "", word)
             word = re.sub(r"\'re", "", word)
             word = re.sub(r"\'d", "", word)
             word = re.sub(r"\'ll", "", word)
             word = re.sub(f"[{re.escape(punctuation)}]", "", word)
-            return word.capitalize()
+            self.sanitised_word = word
         
-        return word.capitalize()
+        self.sanitised_word = word
+        return self.sanitised_word.capitalize()
